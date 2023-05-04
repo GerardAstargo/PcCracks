28065ada22cc8a943030a76e66ad9169c31af4b3
GerardAstargo <ger.astargo@duocuc.cl>
2023-05-04 17:33:09 -0400

    cambio javascript

--- PCCRACKS/js/verificacion_registro.js
+++ PCCRACKS/js/verificacion_registro.js
@@ -1,64 +1,49 @@
-$(document).ready(function(){
-    $("#register").submit(function(e){
-        e.preventDefault();
-        var nombre = $("#name").val();
-        var correo = $("#number").val();
-        var correo = $("#email").val();
-        var correo = $("#password").val();
+var nombre = document.getElementById("name");
+var correo = document.getElementById("email");
+var clave = document.getElementById("password");
+var celular = document.getElementById("number");
+
+const formulario = document.getElementById("register");
 
 
-        let msjMostrar = "";
-        let enviar = false;
+var msj = document.getElementById("warnings");
 
-        if(nombre.trim().lenght < 5 || nombre.trim().lenght > 10){
-            msjMostrar += "El nombre debe tener entre 5 y 10 caracteres";
-        }
+formulario.addEventListener('submit',e =>{
+    let msjMostrar = "";
+    let enviar = false;
 
-        
-        var letra = nombre.trim().charAt(0);
-        if(!esMayuscula(letra)){
+    e.preventDefault();
+
+    if(nombre.value.length < 5 || nombre.value.length > 15){
+        msjMostrar = msjMostrar + "El nombre debe tener entre 5 y 15 caracteres, solo letras";
+        enviar = true;
+    }
+
+    var letra = nombre.value.charAt(0);
+    if(!esMayuscula(letra)){
         msjMostrar += "<br>La primera letra debe ser mayúscula";
         enviar = true;
-        }
-
-        if(enviar){
-            $("#warnings").html(msjMostrar)
-        }
-        else{
-            $("#warnings").html("Cuenta creada correctamente")
-        }
-
-        function esMayuscula(letra){
-            if(letra == letra.toUpperCase()){
-                return true;
-            }
-            else{
-                return false;
-            }
-    
     }
-});
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-   
+    if (celular.value.length < 8 || celular.value.length > 12){
+        msjMostrar = msjMostrar + "El número debe tener los 12 digitos incluyendo el simbolo +";
+        enviar = true;
+    }
 
+    if(enviar){
+        msj.innerHTML = msjMostrar;
+    }
+    else{
+        msj.innerHTML = "Cuenta creada correctamente!!";
+    }
 
 });
+
+function esMayuscula(letra){
+    if(letra == letra.toUpperCase()){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
