ca31dd09bb29b523536674bcd93fe75e438d7f72
GerardAstargo <ger.astargo@duocuc.cl>
2023-05-04 11:19:47 -0400

    jquery

---
+++ PCCRACKS/js/verificacion_registro.js
@@ -0,0 +1,64 @@
+$(document).click(function(){
+    $("register").submit(function(e){
+        e.preventDefault();
+        var nombre = $("#name").val();
+        var correo = $("#number").val();
+        var correo = $("#email").val();
+        var correo = $("#password").val();
+
+
+        let msjMostrar = "";
+        let enviar = false;
+
+        if(nombre.trim().lenght < 5 || nombre.trim().lenght > 10){
+            msjMostrar += "El nombre debe tener entre 5 y 10 caracteres";
+        }
+
+        
+        var letra = nombre.trim().charAt(0);
+        if(!esMayuscula(letra)){
+        msjMostrar += "<br>La primera letra debe ser mayúscula";
+        enviar = true;
+        }
+
+        if(enviar){
+            $("#warnings").html(msjMostrar)
+        }
+        else{
+            $("#warnings").html("Cuenta creada correctamente")
+        }
+
+        function esMayuscula(letra){
+            if(letra == letra.toUpperCase()){
+                return true;
+            }
+            else{
+                return false;
+            }
+    
+    }
+});
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
+
+
+});
