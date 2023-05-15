c8e1fafa1833ba60faac37df675d515e3f8cabd1
GerardAstargo <127121666+GerardAstargo@users.noreply.github.com>
2023-05-15 11:43:17 -0400

    django

--- pcracks_proyecto/pcracks/views.py
+++ pcracks_proyecto/pcracks/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def menu_off (request):
+    return render(request, 'pcracks/menuOFF.html')
+
+def login (request):
+    return render (request, 'pcracraks/Login.html')
