447da5bc6688eb6dd7ae1a988965efe4631bb954
Martimcito <mmbsoto@gmail.com>
2023-05-15 15:33:19 -0400

    y

--- pcracks_proyecto/pcracks/views.py
+++ pcracks_proyecto/pcracks/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def menu_off (request):
-    return render(request, 'pcracks/menuOFF.html')
+        nombre="Nicolas"
+
+    contexto={
+        "nomb": nombre
+    }
+    return render(request, 'pcracks/menuOFF.html',contexto)
 
 def login (request):
     return render (request, 'pcracks/Login.html')
