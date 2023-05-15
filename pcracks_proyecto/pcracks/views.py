c38b5fc35eb758f1067d7bcac3c0fa81126fe64b
Nicolas <103793007+Nyzt@users.noreply.github.com>
2023-05-15 12:02:11 -0400

    contexto

--- pcracks_proyecto/pcracks/views.py
+++ pcracks_proyecto/pcracks/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def menu_off (request):
-    return render(request, 'pcracks/menuOFF.html')
+    nombre="Nicolas"
+
+    contexto={
+        "nomb": nombre
+    }
+    return render(request, 'pcracks/menuOFF.html',contexto)
 
 def login (request):
-    return render (request, 'pcracks/Login.html')
+    
+    return render (request, 'pcracraks/Login.html')
