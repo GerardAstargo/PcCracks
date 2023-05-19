b333bfc33bb6301c601a834485ad6958dedf63d6
GerardAstargo <ger.astargo@duocuc.cl>
2023-05-18 21:15:53 -0400

    arreglado

--- pcracks_proyecto/pcracks/urls.py
+++ pcracks_proyecto/pcracks/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from .views import menu_off, login
 
 urlpatterns = [
-    path('jola', menu_off, name='menu_off'),
-    path('', login, name='login'),
+    path('', menu_off, name='menu_off'),
+    path('asd', login, name='login'),
 ]
