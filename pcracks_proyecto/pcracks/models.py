2b6a2cbc1fe95cff3290d2cdc3083dba9b2d4189
GerardAstargo <ger.astargo@duocuc.cl>
2023-05-22 10:54:51 -0400

    basedd

--- pcracks_proyecto/pcracks/models.py
+++ pcracks_proyecto/pcracks/models.py
@@ -1,15 +1,39 @@
 from django.db import models
 
 # Create your models here.
-"""
-class Raza(models.Model):
-    codigoRaza = models.AutoField(primary_key=True)
-    nombreRaza= models.CharField(max_length=15)
 
-class Mascota(models.Model):
-  codigoChip = models.CharField(max_length=20, primary_key=True)
-  nombreMascota = models.CharField(max_length=25,verbose_name='nombreDelaMascota')
-  edadMascota = models.IntegerField()
-  foto = models.ImageField(upload_to="mascotas")
-  raza = models.ForeignKey(Raza,on_delete=models.CASCADE)
-"""
+
+#BASE CLIENTE
+class Cliente(models.Model):
+  rut_cliente = models.CharField(max_length=15, primary_key=True)
+  nombre_cliente = models.CharField(max_length=15)
+  fecha_nacimiento = models.DateField()
+  direccion_cliente = models.CharField(max_length=30)
+  email_cliente = models.CharField(max_length=20)
+  num_telefonico_cliente = models.IntegerField()
+  
+
+#BASE COMPRA
+class Compra(models.Model):
+  compra_id = models.IntegerField(primary_key=True)
+  metodo_pago = models.CharField(max_length=20)
+  cantidad_productos = models.IntegerField()
+  total_compra = models.IntegerField()
+  fecha_compra = models.DateField()
+  cliente = models.ForeignKey(Cliente,on_delete=models.CASCADE)
+
+#BASE PRODUCTO
+class Producto (models.Model):
+  marca = models.CharField(max_length=20)
+  modelo = models.CharField(max_length=20)
+  precio = models.IntegerField()
+  compra = models.ForeignKey(Compra,on_delete=models.CASCADE)
+
+#BASE EMPLEADO
+class Empleado (models.Model):
+  rut_empleado = models.CharField(max_length=15, primary_key=True)
+  nombre_empleado = models.CharField(max_length=15)
+  fecha_nacimiento = models.DateField()
+  direccion_empleado = models.CharField(max_length=30)
+  email_empleado = models.CharField(max_length=20)
+  num_telefonico_empleado = models.IntegerField()
