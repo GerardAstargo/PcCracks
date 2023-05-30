09307825d9f37bf24022664fa6169f7661825641
ivanjhm <103793007+Nyzt@users.noreply.github.com>
2023-05-30 09:36:45 -0400

    1

--- pcracks_proyecto/pcracks_proyecto/settings.py
+++ pcracks_proyecto/pcracks_proyecto/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-MESSAGE_STORAGE = "django.contrib.messges.storage.cookie.CookieStorage"
+MESSAGE_STORAGE = "django.contrib.messages.storage.cookie.CookieStorage"
 
 # Application definition
 
