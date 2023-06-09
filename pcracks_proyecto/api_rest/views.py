455b0688c0a5a20bfaaab7db6f1b116d56f6f94d
GerardAstargo <ger.astargo@duocuc.cl>
2023-06-09 11:02:41 -0400

    put delete

--- pcracks_proyecto/api_rest/views.py
+++ pcracks_proyecto/api_rest/views.py
@@ -23,3 +23,22 @@ def lista_clientes(request):
             return Response(serializer.data,status= status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors,status= status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def detalle_cliente(request, id):
+
+    try:
+        cliente = Cliente.objects.get(Rut = id)
+    except Cliente.DoesNotExist:
+        return Response(status = status.HTTP_404_NOT_FOUND)
+    if request.method == 'PUT':
+        data = JSONParser().parse(request)
+        seralizer = ClienteSerializers ( cliente, data = data)
+        if seralizer.is_valid():
+            seralizer.save()
+            return Response(seralizer.data)
+        else:
+            return Response(seralizer.errors,status= status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        cliente.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
