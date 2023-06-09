a1ddfc0e2c6d276516c34aeee4586d8fa6b8483a
Nicolas <103793007+Nyzt@users.noreply.github.com>
2023-06-09 11:15:04 -0400

    xs

--- pcracks_proyecto/api_rest/urls.py
+++ pcracks_proyecto/api_rest/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from api_rest.views import lista_clientes
+from api_rest.views import lista_clientes,detalle_cliente
 
 urlpatterns = [
-    path('lista_clientes/', lista_clientes, name='lista_clientes')
+    path('lista_clientes/', lista_clientes, name='lista_clientes'),
+    path('detalle_cliente/<id>', detalle_cliente, name='detalle_cliente'),
 ]
